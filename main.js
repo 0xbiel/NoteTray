289285d8cae05c66b11392edaadc673fe2369c79
Gabriel Polastrini <bielp07@gmail.com>
2019-07-24 10:55:00 -0300

    Window showing on tray icon position!

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow, Tray} = require('electron')
 const path = require('path')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 let tray = null;
 
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 400,
-    height: 400,
+    height: 500,
+    skipTaskbar: true,
+    frame: false,
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  mainWindow.setMenu(null);
+  mainWindow.setMenu(null)
 
-  tray = new Tray("./assets/icon@2x.png");
+  mainWindow.hide();
+
+  tray = new Tray("./assets/icon@2x_white.png");
 
   tray.on('click', () => {
     mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
   })
   mainWindow.on('show', () => {
     tray.setHighlightMode('always')
+
+    const pos = tray.getBounds()
+
+    mainWindow.setPosition(pos.x, pos.y);
   })
   mainWindow.on('hide', () => {
     tray.setHighlightMode('never')
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
@@ -51,18 +48,12 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+ 
   if (mainWindow === null) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
